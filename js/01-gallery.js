9fe4b2ca7266c0d62283c4fe738c4239193f33ef
OleksandrR61 <a80953031761@gmail.com>
2022-10-10 22:12:14 +0300

    HW-JS#7v0.2
    Update 01-gallery.js: added create gallery and starting create open modal.

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+galleryRef.insertAdjacentHTML("beforeend", createGallery(galleryItems));
+galleryRef.addEventListener('click', handleClick);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(item => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${item.original}">
+                        <img
+                            class="gallery__image"
+                            src="${item.preview}"
+                            data-source="${item.original}"
+                            alt="${item.description}"
+                        />
+                    </a>
+                </div>`;
+    }).join('');
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    
+    console.log(event.target.dataset.source);
+}
